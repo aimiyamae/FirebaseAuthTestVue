87c3da0b2a4c67e76bbb569b00366d590aa80794
Kingfisher <kingfisherai306@gmail.com>
2019-02-19 18:22:55 +0900

    インデント訂正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,67 +1,75 @@
 <template>
-
-	<div class='hello'>
-			<v-btn @click="signOut" outline color="indigo">Sign out</v-btn>
-		<div class="text-xs-center">
-			<v-badge color="purple" left overlap >
-				<v-icon slot="badge" dark small >done</v-icon>
-				<v-icon color="grey lighten-1" large > account_circle </v-icon>
-			</v-badge>
-		</div>
-		<h1>{{ msg }}</h1>
-		<h3>{{ name }}</h3>
-		<div class="text-xs-center">
-			<!-- golangニュースページに飛ぶ -->
-			<v-btn @click="hrefURL" outline color="indigo">NEWS</v-btn>
-			<v-btn @click="reservationForm" outline color="indigo">予約フォーム</v-btn>
-			<v-btn @click="apiPrivate" outline color="indigo">private商品一覧</v-btn>
-			<v-btn @click="myPage" outline color="indigo">マイページ</v-btn>
-		</div>
-		<!-- <div v-show="state=='notLoggedIn'">
-			<button @click="loginWithEmail">ログインtest</button> -->
-		<!-- </div> -->
-		
-
-				<template>
-    <div class="signin">
-		<v-content>
-			<v-container fluid fill-height>
-			<v-layout align-center justify-center>
-				<v-flex xs12 sm8 md4>
-				<v-card class="elevation-12">
-					<v-toolbar color="#2ECCFA">
-					<v-toolbar-title >マイページ</v-toolbar-title>
-					<v-spacer>
-					</v-spacer>
-					</v-toolbar>
-					<v-card-text>
-						<v-form>
-							<p>{{`登録ナンバー: ${auth.phoneNumber}`}}</p>
-							<v-card-actions>
-								
-								<v-spacer></v-spacer>
-								<h4 class="headline mb-5">{{`08053195532(仮)${auth.phoneNumber}`}}</h4>
-								<v-btn color="#EEEEEE" @click="transferAction">振込申請</v-btn>
-							</v-card-actions>
-							<v-btn flat color="black">取り扱い対象外アイテム</v-btn><br />
-							<v-btn flat color="black">利用規約</v-btn><br />
-							<v-btn flat color="black">プライバシーポリシー</v-btn><br />
-							<v-btn flat color="black">古物営業法に基づく表記</v-btn><br />
-							<v-btn flat color="black">運営会社</v-btn><br />
-							<v-btn @click="inquiryForm" flat color="black">お問い合わせ</v-btn><br />
-						</v-form>
-					</v-card-text>
-					<!-- <v-card-actions>
+  <div class="hello">
+    <v-btn @click="signOut" outline color="indigo">Sign out</v-btn>
+    <div class="text-xs-center">
+      <v-badge color="purple" left overlap>
+        <v-icon slot="badge" dark small>done</v-icon>
+        <v-icon color="grey lighten-1" large>account_circle</v-icon>
+      </v-badge>
+    </div>
+    <h1>{{ msg }}</h1>
+    <h3>{{ name }}</h3>
+    <div class="text-xs-center">
+      <!-- golangニュースページに飛ぶ -->
+      <v-btn @click="hrefURL" outline color="indigo">NEWS</v-btn>
+      <v-btn @click="reservationForm" outline color="indigo">予約フォーム</v-btn>
+      <v-btn @click="apiPrivate" outline color="indigo">private商品一覧</v-btn>
+      <v-btn @click="myPage" outline color="indigo">マイページ</v-btn>
+      <v-btn @click="apiPublic" outline color="indigo">golangテスト</v-btn>
+    </div>
+    <!-- <div v-show="state=='notLoggedIn'">
+    <button @click="loginWithEmail">ログインtest</button>-->
+    <!-- </div> -->
+    <template>
+      <div class="signin">
+        <v-content>
+          <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+              <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                  <v-toolbar color="#2ECCFA">
+                    <v-toolbar-title>マイページ</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                  </v-toolbar>
+                  <v-card-text>
+                    <v-form>
+                      <p>{{`登録ナンバー: ${auth.phoneNumber}`}}</p>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <h4 class="headline mb-5">{{`08053195532(仮)${auth.phoneNumber}`}}</h4>
+                        <v-btn color="#EEEEEE" @click="transferAction">振込申請</v-btn>
+                      </v-card-actions>
+                      <v-btn flat color="black">取り扱い対象外アイテム</v-btn>
+                      <br>
+                      <v-btn flat color="black">利用規約</v-btn>
+                      <br>
+                      <v-btn flat color="black">プライバシーポリシー</v-btn>
+                      <br>
+                      <v-btn flat color="black">古物営業法に基づく表記</v-btn>
+                      <br>
+                      <v-btn flat color="black">運営会社</v-btn>
+                      <br>
+                      <v-btn @click="inquiryForm" flat color="black">お問い合わせ</v-btn>
+                      <br>
+                      <div class="form-group mb-0">
+                        <label>日付選択</label>
+                        <input v-model="birthday" type="date" class="form-control">
+                      </div>
+                    </v-form>
+                  </v-card-text>
+                  <!-- <v-card-actions>
 						<v-spacer>
 						</v-spacer>
 						<v-btn color="#EEEEEE" @click="signIn">振込申請</v-btn>
-					</v-card-actions> -->
-				</v-card>
-				</v-flex>
-			</v-layout>
-			</v-container>
-		</v-content>
-    </div>
+                  </v-card-actions>-->
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-content>
+      </div>
+    </template>
+  </div>
 </template>
 		<!-- <div id="app">
 			<h1>{{ msgCalendar }}</h1>
@@ -83,128 +91,154 @@
 </template>
 
 <script>
-
-import axios from 'axios'
-import firebase from 'firebase'
+import axios from "axios";
+import firebase from "firebase";
 export default {
-name: 'HelloWorld',
-data () {
-	return {
-		// picker: new Date().toISOString().substr(0, 10),
-		msg: 'マイページ',
-		// msgCalendar: '日付を選択',
-		name: firebase.auth().currentUser.uid,
-		phoneNumber: '',
-		auth: {
-			email: 'test@gmail.com',
-			phoneNumber: '',
-		},
-		recaptchaVerifier: null,
-		confirmationResult: null,
-		waitingVerify: false,
-		verificationCode: '',
-		user: null
-	}
-},
-computed: {
+  name: "HelloWorld",
+  data() {
+    return {
+      // picker: new Date().toISOString().substr(0, 10),
+      msg: "マイページ",
+      // msgCalendar: '日付を選択',
+      name: firebase.auth().currentUser.uid,
+      phoneNumber: "",
+      auth: {
+        email: "test@gmail.com",
+        phoneNumber: ""
+      },
+      recaptchaVerifier: null,
+      confirmationResult: null,
+      waitingVerify: false,
+      verificationCode: "",
+      user: null
+    };
+  },
+  computed: {
     state() {
-		if (!this.waitingVerify && !this.auth.phoneNumber) {
-			return 'onlyEmail'
-		}
-		if (this.waitingVerify && !this.auth.phoneNumber) {
-			return 'waitingVerify'
-		}
-			return 'emailAndPhoneNumber'
-		},
-	},
-	async mounted() {
-		const _this = this
-		firebase.auth().getRedirectResult().then((result) => {
-			if (result.credential) {
-				const user = result.user
-				_this.user = user
-				if (user.email) {
-				_this.auth.email = user.email
-				}
-			}
-		})
+      if (!this.waitingVerify && !this.auth.phoneNumber) {
+        return "onlyEmail";
+      }
+      if (this.waitingVerify && !this.auth.phoneNumber) {
+        return "waitingVerify";
+      }
+      return "emailAndPhoneNumber";
+    }
+  },
+  async mounted() {
+    const _this = this;
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(result => {
+        if (result.credential) {
+          const user = result.user;
+          _this.user = user;
+          if (user.email) {
+            _this.auth.email = user.email;
+          }
+        }
+      });
 
-		this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('linkPhoneNumberBtn', {
-		'size': 'invisible',
-		})
-	},
-	methods: {
-		signOut: function () {
-			firebase.auth().signOut().then(() => {
-				localStorage.removeItem('jwt')
-				this.$router.push('/signin')
-			})
-		},
-		transferAction: function () {
-			open( "https://docs.google.com/forms/d/e/1FAIpQLSc4fFXAKvTWGaY2oE2gSxSMls9S7pxNePcxOMF6-9NRa5tbvQ/viewform", "_blank" ) ;
-		},
-		myPage: function () {
-			// window.location.href='http://localhost:8080/news'
-			this.$router.push('/')
-		},
-		hrefURL: function () {
-			// window.location.href='http://localhost:8080/news'
-			this.$router.push('/news')
-		},
-		inquiryForm: function () {
-			open( "https://docs.google.com/forms/d/1riQvax43mOky4luvP8K5AKPZNvYEJ3umOcHY_teXTWg/edit", "_blank" ) ;
-			// window.location.href='https://docs.google.com/forms/d/1riQvax43mOky4luvP8K5AKPZNvYEJ3umOcHY_teXTWg/edit'
-		},
-		reservationForm: async function () {
-			open( "https://docs.google.com/forms/d/e/1FAIpQLSdO02JHSaXkqgsDo8gKJR0oaHDCZs5eWZbmc_1de2kf7g2tNA/viewform", "_blank" ) ;
-		},
-		apiPrivate: async function () {
-			let res = await axios.get('http://localhost:8000/private', {
-				headers: {'Authorization': `Bearer ${localStorage.getItem('jwt')}`}
-			})
-			this.msg = res.data
-		},
-		loginWithEmail() {
-			const provider = new firebase.auth.GoogleAuthProvider()
-			firebase.auth().signInWithRedirect(provider)
-		},
-		async sendSmsVerification() {
-			try {
-				const confirmationResult = await firebase.auth().signInWithPhoneNumber(this.phoneNumber, this.recaptchaVerifier)
-				this.confirmationResult = confirmationResult
-				this.waitingVerify = true
-			} catch(error) {
-				// console.error(error)
-			}
-		},
-		async confirmVerification() {
-			const credential = firebase.auth.PhoneAuthProvider.credential(this.confirmationResult.verificationId, this.verificationCode)
-			const userCred = await this.user.linkAndRetrieveDataWithCredential(credential)
-			this.auth.phoneNumber = userCred.user.phoneNumber
-		},
-		//カレンダー選択不可の式
-		allowedDates: function(val){
-			// 今日～100日後までを選べるようにする
-			let today = new Date()
-			today = new Date(
-				today.getFullYear(),
-				today.getMonth(),
-				today.getDate() + 1
-			)
-			let maxAllowedDay = new Date()
-			maxAllowedDay.setDate(
-				today.getDate() + 100
-			)
-			maxAllowedDay = new Date(
-				maxAllowedDay.getFullYear(),
-				maxAllowedDay.getMonth(),
-				maxAllowedDay.getDate()
-			)
-			return today <= new Date(val) && new Date(val) <= maxAllowedDay
-		}
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "linkPhoneNumberBtn",
+      {
+        size: "invisible"
+      }
+    );
+  },
+  methods: {
+    signOut: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          localStorage.removeItem("jwt");
+          this.$router.push("/signin");
+        });
+    },
+    transferAction: function() {
+      open(
+        "https://docs.google.com/forms/d/e/1FAIpQLSc4fFXAKvTWGaY2oE2gSxSMls9S7pxNePcxOMF6-9NRa5tbvQ/viewform",
+        "_blank"
+      );
+    },
+    myPage: function() {
+      // window.location.href='http://localhost:8080/news'
+      this.$router.push("/");
+    },
+    hrefURL: function() {
+      // window.location.href='http://localhost:8080/news'
+      this.$router.push("/news");
+    },
+    inquiryForm: function() {
+      open(
+        "https://docs.google.com/forms/d/1riQvax43mOky4luvP8K5AKPZNvYEJ3umOcHY_teXTWg/edit",
+        "_blank"
+      );
+      // window.location.href='https://docs.google.com/forms/d/1riQvax43mOky4luvP8K5AKPZNvYEJ3umOcHY_teXTWg/edit'
+    },
+    reservationForm: async function() {
+      open(
+        "https://docs.google.com/forms/d/e/1FAIpQLSdO02JHSaXkqgsDo8gKJR0oaHDCZs5eWZbmc_1de2kf7g2tNA/viewform",
+        "_blank"
+      );
+    },
+    apiPublic: async function() {
+      let res = await axios.get("http://localhost:8000/public");
+      this.msg = res.data;
+    },
+    apiPrivate: async function() {
+      let res = await axios.get("http://localhost:8000/private", {
+        headers: { Authorization: `Bearer ${localStorage.getItem("jwt")}` }
+      });
+      this.msg = res.data;
+    },
+    loginWithEmail() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    },
+    async sendSmsVerification() {
+      try {
+        const confirmationResult = await firebase
+          .auth()
+          .signInWithPhoneNumber(this.phoneNumber, this.recaptchaVerifier);
+        this.confirmationResult = confirmationResult;
+        this.waitingVerify = true;
+      } catch (error) {
+        // console.error(error)
+      }
+    },
+    async confirmVerification() {
+      const credential = firebase.auth.PhoneAuthProvider.credential(
+        this.confirmationResult.verificationId,
+        this.verificationCode
+      );
+      const userCred = await this.user.linkAndRetrieveDataWithCredential(
+        credential
+      );
+      this.auth.phoneNumber = userCred.user.phoneNumber;
+    },
+    //カレンダー選択不可の式
+    allowedDates: function(val) {
+      // 今日～100日後までを選べるようにする
+      let today = new Date();
+      today = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 1
+      );
+      let maxAllowedDay = new Date();
+      maxAllowedDay.setDate(today.getDate() + 100);
+      maxAllowedDay = new Date(
+        maxAllowedDay.getFullYear(),
+        maxAllowedDay.getMonth(),
+        maxAllowedDay.getDate()
+      );
+      return today <= new Date(val) && new Date(val) <= maxAllowedDay;
+    }
     // allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0
-  	}
-}
+  }
+};
 
 // import axios from 'axios'
 // import firebase from 'firebase'
--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,161 +1,217 @@
 <template>
-    <div class="signin">
-		<v-content>
-			<v-container fluid fill-height>
-			<v-layout align-center justify-center>
-				<v-flex xs12 sm8 md4>
-				<v-card class="elevation-12">
-					<v-toolbar color="#EEEEEE">
-					<v-toolbar-title>Login form</v-toolbar-title>
-					<v-spacer></v-spacer>
-					</v-toolbar>
-					<v-card-text>
-            <h3>電話番号を入力</h3>
-			<input v-model='phoneNumber' />
-			<button id='linkPhoneNumberBtn' @click="sendSmsVerification">確認番号を送信</button>
-					<v-form>
-            <v-text-field prepend-icon="email" name="email" label="電話ラベル" type="number" v-model="email"></v-text-field>
-						<v-text-field prepend-icon="email" name="email" label="Email" type="text" v-model="email"></v-text-field>
-						<v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" v-model="password"></v-text-field>
-					</v-form>
-					</v-card-text>
-					<v-card-actions>
-					<v-spacer></v-spacer>
-					<v-btn color="#EEEEEE" @click="signIn">Sign in</v-btn>
-          <v-btn id='linkPhoneNumberBtn' @click="sendSmsVerification">確認番号を送信</v-btn>
-          <v-btn @click="confirmVerification">確認</v-btn>
-					</v-card-actions>
-				</v-card>
-				</v-flex>
-			</v-layout>
-			</v-container>
-		</v-content>
-		<v-btn  @click="authGoogle" outline midium fab color="black">
-			<i class="fab fa-google fa-2x"></i>
-		</v-btn>
-		<v-btn  @click="authFaceBook" outline midium fab color="black">
-			<i class="fab fa-facebook fa-2x"></i>
-		</v-btn>
-		<v-btn  @click="authTwitter" outline midium fab color="black">
-			<i class="fab fa-twitter-square fa-2x"></i>
-		</v-btn>
-		<p>You don't have an account?
-			<router-link to="/signup">create account now!!</router-link>
-		</p>
-    </div>
+  <div class="signin">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar color="#EEEEEE">
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <h3>電話番号を入力</h3>
+                <input v-model="phoneNumber">
+                <button id="linkPhoneNumberBtn" @click="sendSmsVerification">確認番号を送信</button>
+                <v-form>
+                  <v-text-field
+                    prepend-icon="email"
+                    name="email"
+                    label="電話ラベル"
+                    type="number"
+                    v-model="email"
+                  ></v-text-field>
+                  <v-text-field
+                    prepend-icon="email"
+                    name="email"
+                    label="Email"
+                    type="text"
+                    v-model="email"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    prepend-icon="lock"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    v-model="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="#EEEEEE" @click="signIn">Sign in</v-btn>
+                <v-btn id="linkPhoneNumberBtn" @click="sendSmsVerification">確認番号を送信</v-btn>
+                <v-btn @click="confirmVerification">確認</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-btn @click="authGoogle" outline midium fab color="black">
+      <i class="fab fa-google fa-2x"></i>
+    </v-btn>
+    <v-btn @click="authFaceBook" outline midium fab color="black">
+      <i class="fab fa-facebook fa-2x"></i>
+    </v-btn>
+    <v-btn @click="authTwitter" outline midium fab color="black">
+      <i class="fab fa-twitter-square fa-2x"></i>
+    </v-btn>
+    <p>You don't have an account?
+      <router-link to="/signup">create account now!!</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from "firebase";
 export default {
-	name: 'Signin',
-	data: function () {
-		return {
-			email: '',
-			password: '',
-			auth: {
-				email: 'test@gmail.com',
-				phoneNumber: '',
-			},
-			phoneNumber: '',
-			recaptchaVerifier: null,
-			confirmationResult: null,
-			waitingVerify: false,
-			verificationCode: '',
-			user: null
-		}
-	},
-	computed: {
+  name: "Signin",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      auth: {
+        email: "test@gmail.com",
+        phoneNumber: ""
+      },
+      phoneNumber: "",
+      recaptchaVerifier: null,
+      confirmationResult: null,
+      waitingVerify: false,
+      verificationCode: "",
+      user: null
+    };
+  },
+  computed: {
     state() {
-		if (!this.auth.email) {
-			return 'notLoggedIn'
-		}
-		if (this.auth.email  && !this.waitingVerify && !this.auth.phoneNumber) {
-			return 'onlyEmail'
-		}
-		if (this.auth.email && this.waitingVerify && !this.auth.phoneNumber) {
-			return 'waitingVerify'
-		}
-			return 'emailAndPhoneNumber'
-		},
-	},
-	async mounted() {
-		const _this = this
-		firebase.auth().getRedirectResult().then((result) => {
-			if (result.credential) {
-				const user = result.user
-				_this.user = user
-				if (user.phoneNumber) {
-				_this.auth.phoneNumber = user.phoneNumber
-				}
-			}
-		})
+      if (!this.auth.email) {
+        return "notLoggedIn";
+      }
+      if (this.auth.email && !this.waitingVerify && !this.auth.phoneNumber) {
+        return "onlyEmail";
+      }
+      if (this.auth.email && this.waitingVerify && !this.auth.phoneNumber) {
+        return "waitingVerify";
+      }
+      return "emailAndPhoneNumber";
+    }
+  },
+  async mounted() {
+    const _this = this;
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(result => {
+        if (result.credential) {
+          const user = result.user;
+          _this.user = user;
+          if (user.phoneNumber) {
+            _this.auth.phoneNumber = user.phoneNumber;
+          }
+        }
+      });
 
-		this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('linkPhoneNumberBtn', {
-		'size': 'invisible',
-		})
-	},
-	methods: {
-		async sendSmsVerification() {
-			console.log(this.phoneNumber)
-			try {
-				const confirmationResult = await firebase.auth().signInWithPhoneNumber(this.phoneNumber, this.recaptchaVerifier)
-				this.confirmationResult = confirmationResult
-				this.waitingVerify = true
-				console.log(this.this.confirmationResult)
-			} catch(error) {
-				// console.error(error)
-			}
-		},
-		async confirmVerification() {
-			const credential = firebase.auth.PhoneAuthProvider.credential(this.confirmationResult.verificationId, this.verificationCode)
-			const userCred = await this.user.linkAndRetrieveDataWithCredential(credential)
-			this.auth.phoneNumber = userCred.user.phoneNumber
-		},
-		signIn: function () {
-			firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(res => {
-				res.user.getIdToken().then(idToken => {
-				localStorage.setItem('jwt', idToken.toString())
-				})
-				this.$router.push('/')
-			}, err => {
-				alert(err.message)
-			})
-		},
-		authGoogle: function () {
-			const provider = new firebase.auth.GoogleAuthProvider()
-			firebase.auth().signInWithPopup(provider).then(res => {
-				res.user.getIdToken().then(idToken => {
-				localStorage.setItem('jwt', idToken.toString())
-				})
-				this.$router.push('/')
-			}, err => {
-				alert(err.message)
-			})
-		},
-		authTwitter: function () {
-			const provider = new firebase.auth.TwitterAuthProvider()
-			firebase.auth().signInWithPopup(provider).then(res => {
-				res.user.getIdToken().then(idToken => {
-				localStorage.setItem('jwt', idToken.toString())
-				})
-				this.$router.push('/')
-			}, err => {
-				alert(err.message)
-			})
-		},
-		
-		authFaceBook: function () {
-			const provider = new firebase.auth.FacebookAuthProvider()
-			firebase.auth().signInWithPopup(provider).then(res => {
-				res.user.getIdToken().then(idToken => {
-					localStorage.setItem('jwt', idToken.toString())
-				})
-				this.$router.push('/')
-			}, err => {
-				alert(err.message)
-			})
-		}
-  	}
-}
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "linkPhoneNumberBtn",
+      {
+        size: "invisible"
+      }
+    );
+  },
+  methods: {
+    async sendSmsVerification() {
+      console.log(this.phoneNumber);
+      try {
+        const confirmationResult = await firebase
+          .auth()
+          .signInWithPhoneNumber(this.phoneNumber, this.recaptchaVerifier);
+        this.confirmationResult = confirmationResult;
+        this.waitingVerify = true;
+        console.log(this.this.confirmationResult);
+      } catch (error) {
+        // console.error(error)
+      }
+    },
+    async confirmVerification() {
+      const credential = firebase.auth.PhoneAuthProvider.credential(
+        this.confirmationResult.verificationId,
+        this.verificationCode
+      );
+      const userCred = await this.user.linkAndRetrieveDataWithCredential(
+        credential
+      );
+      this.auth.phoneNumber = userCred.user.phoneNumber;
+    },
+    signIn: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          res => {
+            res.user.getIdToken().then(idToken => {
+              localStorage.setItem("jwt", idToken.toString());
+            });
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    },
+    authGoogle: function() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(
+          res => {
+            res.user.getIdToken().then(idToken => {
+              localStorage.setItem("jwt", idToken.toString());
+            });
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    },
+    authTwitter: function() {
+      const provider = new firebase.auth.TwitterAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(
+          res => {
+            res.user.getIdToken().then(idToken => {
+              localStorage.setItem("jwt", idToken.toString());
+            });
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    },
+
+    authFaceBook: function() {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(
+          res => {
+            res.user.getIdToken().then(idToken => {
+              localStorage.setItem("jwt", idToken.toString());
+            });
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+    }
+  }
+};
 </script>
